e8af2a35f88f9a4fafb52b03f80dd53cfeb57d9f
BearYeWon <bear100152@gmail.com>
2026-01-07 18:08:35 +0900

    feat: font 및 color data class 추가, theme object 정의

--- app/src/main/java/com/example/hackathon/ui/theme/Theme.kt
+++ app/src/main/java/com/example/hackathon/ui/theme/Theme.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme =
@@ -64,4 +65,16 @@ fun HackathonTheme(
         typography = Typography,
         content = content,
     )
+}
+
+object HackathonTheme{
+    val colors: HackathonColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalHackathonColors
+
+    val typography: HackathonTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalHackathonTypography
 }
--- app/src/main/java/com/example/hackathon/ui/theme/Type.kt
+++ app/src/main/java/com/example/hackathon/ui/theme/Type.kt
@@ -1,9 +1,11 @@
 package com.example.hackathon.ui.theme
 
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.hackathon.R
 
@@ -88,5 +90,30 @@ val defaultHackathonTypography = HackathonTypography(
     ),
 )
 
-val LocalHackathonTypography = staticCompositionLocalOf { defaultHackathonTypography }
+val LocalHackathonTypography: HackathonTypography = staticCompositionLocalOf { defaultHackathonTypography }
 
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
